9173e4138df9bc87ad167d7a13bb6e77e0fbb3bb
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-23 11:38:11 -0600

    Complete | ShortLongShort

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,12 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+        String a = args[0];
+        String b = args[1];
+
+        if (a.length() > b.length()) {
+            System.out.println(b+a+b);
+        } else {
+            System.out.println(a+b+a);
+        }
     }
 }

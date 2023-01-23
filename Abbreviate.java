0a1ef45b9282ab7920b3289e59e433c7da7a0288
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-23 11:48:28 -0600

    Complete | Abbreviate

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,13 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        String a = args[0];
+        String firstTwo = a.substring(0,2);
+        String lastTwo = a.substring(a.length()-2, a.length());
+
+        if (a.length()<8) {
+            System.out.println(a);
+        } else {
+            System.out.println(firstTwo+"..."+lastTwo);
+        }
     }
 }

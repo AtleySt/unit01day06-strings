1a8236e6902d553050ff2c9d4a355aa5a3af8817
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-23 11:43:45 -0600

    Complete | NegatePhrase

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,12 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        String a = args[0];
+        String firstThree = a.substring(0,3);
+
+        if (firstThree.equals("not")) {
+            System.out.println(a);
+        } else {
+            System.out.println("not " + a);
+        }
     }
 }
